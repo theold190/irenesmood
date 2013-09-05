820f36553706c3db3d8e213d0fbcc445bd9bb931
TheOld <the.old@gmail.com>
2013-09-05 21:57:40 +0200

    Now mood could be updated on page load

---
+++ Extension/js/popup.js
@@ -0,0 +1,12 @@
+function updateMood() {
+	var image = document.getElementById('IDMoodPicture');
+	var text = document.getElementById('IDMoodText');
+	//TODO: Update mood and text here
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+	updateMood();
+});
